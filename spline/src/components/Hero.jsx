103676b80981963d33eb7205d0a2d083d3dfe3c5
deepakwork360 <deepakwork360@gmail.com>
2025-09-22 01:40:39 +0530

    Fix vite base path and add Netlify redirects

--- spline/src/components/Hero.jsx
+++ spline/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import "boxicons/css/boxicons.min.css";
 import Spline from '@splinetool/react-spline'
 
+
 const Hero = () => {
   return (
     <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
